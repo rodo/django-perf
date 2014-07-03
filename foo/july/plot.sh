9afba57f72292cd4e6356e1069311fb807e74795
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2014-07-03 15:02:20 +0200

    Add resuls

--- foo/july/plot.sh
+++ foo/july/plot.sh
@@ -4,10 +4,14 @@ set terminal png
 set xlabel "time"
 set xdata time
 set timefmt "%d-%m %H:%M:%S"
-set format x "%M:%S"
+set yrange [0:500]
+set y2range [0:100]
 set title "IO requests"
 set ylabel "requests"
-set output "io-requests.png"
+set y2label "cpu iowait"
+set xtics font "Verdana,8"
+set ytics font "Verdana,8"
 set datafile separator ","
-plot "dstat.dat" every ::8 using 1:2  title "io read" with lines, \
- "dstat.dat" every ::8 using 1:3  title "io write" with lines
+set output "io-requests4.png"
+plot "dstat4.dat" every ::7 using 1:2 axes x1y1 title "io read" with lines, \
+    "dstat4.dat" every ::7 using 1:9 axes x1y2 title "io wait" with lines
