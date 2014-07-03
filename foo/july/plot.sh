f54011fe8084b9340694701c007855b3f9dbb435
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2014-07-03 15:30:04 +0200

    Rewrite

--- foo/july/plot.sh
+++ foo/july/plot.sh
@@ -4,14 +4,14 @@ set terminal png
 set xlabel "time"
 set xdata time
 set timefmt "%d-%m %H:%M:%S"
-set yrange [0:500]
-set y2range [0:100]
-set title "IO requests"
-set ylabel "requests"
-set y2label "cpu iowait"
+set ylabel "kB/sec"
+set y2label "%"
+set y2range [0:50]
 set xtics font "Verdana,8"
 set ytics font "Verdana,8"
+set y2tics font "Verdana,8"
 set datafile separator ","
+set title "Disk IO - test 4"
 set output "io-requests4.png"
-plot "dstat4.dat" every ::7 using 1:2 axes x1y1 title "io read" with lines, \
-    "dstat4.dat" every ::7 using 1:9 axes x1y2 title "io wait" with lines
+plot "dstat4.dat" every ::5 using 1:($4/1024) axes x1y1 title "disk read" with lines linetype rgb "blue", \
+    "dstat4.dat" every ::5 using 1:9 axes x1y2 title "iowait" with lines
