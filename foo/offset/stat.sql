8fbfe410748c1b2b72dedc502db9998a9a9db8b3
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2014-10-04 19:11:38 +0200

    Add stat

---
+++ foo/offset/stat.sql
@@ -0,0 +1,5 @@
+                                                            ^
+
+select relname, heap_blks_read , heap_blks_hit , idx_blks_read , idx_blks_hit ,toast_blks_read , toast_blks_hit , tidx_blks_read , tidx_blks_hit from pg_statio_user_tables where relname like 'july%';
+
+select pg_size_pretty(pg_database_size('djangoperf'));
